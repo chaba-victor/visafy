e8175b795991b0226b61261d6bd408d97d042b40
Victor Chaba <chabavictor7@gmail.com>
2024-07-17 08:44:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ else:
     # session state
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = [
-            AIMessage(content="Hello, I am a bot. How can I help you?"),
+            AIMessage(content="Hello, I am a Lisa. How can I help you?"),
         ]
     if "vector_store" not in st.session_state:
         st.session_state.vector_store = get_vectorstore_from_url(website_url)    
